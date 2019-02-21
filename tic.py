61547941c737ce3b90dd2da9a88d507cb2929c41
SatyamAnand98 <thesatemail@gmail.com>
2019-02-22 03:49:49 +0530

    Second edit to tic.py

--- tic.py
+++ tic.py
@@ -7,6 +7,7 @@ clear_output()
 
 board_position = ['1','2','3','4','5','6','7','8','9']
 player2 = player1 =  ''
+flag = c = 0
 
 class color:
    PURPLE = '\033[95m'
@@ -19,6 +20,15 @@ class color:
    BOLD = '\033[1m'
    UNDERLINE = '\033[4m'
    END = '\033[0m'
+
+def player_input():
+    marker = ''
+    while not(marker=='X' or marker=='O'):
+        marker = input("Player1: choose X or O: ").upper()
+        if marker =='X':
+            return('X','O')
+        else:
+            return('O','X')
 
 def display_board():
     for i in range(0,9,3):
@@ -55,90 +65,29 @@ def player2_move():
         if(i<6):
             print('--------')
 
+def win_check():
+    return((board_position[0]==board_position[1] and board_position[1]==board_position[2])or
+    (board_position[3]==board_position[4] and board_position[4]==board_position[5])or
+    (board_position[6]==board_position[7] and board_position[7]==board_position[8])or
+    (board_position[0]==board_position[4] and board_position[4]==board_position[8])or
+    (board_position[2]==board_position[4] and board_position[4]==board_position[6])or
+    (board_position[0]==board_position[3] and board_position[3]==board_position[6])or
+    (board_position[1]==board_position[4] and board_position[4]==board_position[7])or
+    (board_position[2]==board_position[5] and board_position[5]==board_position[8]))
+
+
 display_board()
-flag = c = 0
-player2 = ''
 print("Player 1: Choose Your Symbol X or O")
-player1 = input()
-
-if(player1=='X'):
-    player2 = 'O'
-elif(player1=='O'):
-    player2 = 'X'
-else:
-    print("wrong option entered")
-    exit(0)
-
+player1, player2 = player_input()
 
 while(c<10):
     if(flag!=1 and c<9):
         print('\n'*100)
         display_board()
         player1_move()
-        if(board_position[0]==board_position[1] and board_position[1]==board_position[2]):
-            flag = 1
-            if(board_position[0]==player1):
-                print("Player 1 Won!!")
-                break
-            elif(board_position[0]==player2):
-                print("Player 2 Won!!")
-                break
-        if(board_position[3]==board_position[4] and board_position[4]==board_position[5]):
-            flag = 1
-            if(board_position[3]==player1):
-                print("Player 1 Won!!")
-                break
-            elif(board_position[3]==player2):
-                print("Player 2 Won!!")
-                break
-        if(board_position[6]==board_position[7] and board_position[7]==board_position[8]):
-            flag = 1
-            if(board_position[6]==player1):
-                print("Player 1 Won!!")
-                break
-            elif(board_position[6]==player2):
-                print("Player 2 Won!!")
-                break
-        if(board_position[0]==board_position[4] and board_position[4]==board_position[8]):
-            flag = 1
-            if(board_position[0]==player1):
-                print("Player 1 Won!!")
-                break
-            elif(board_position[0]==player2):
-                print("Player 2 Won!!")
-                break
-        if(board_position[2]==board_position[4] and board_position[4]==board_position[6]):
-            flag = 1
-            if(board_position[2]==player1):
-                print("Player 1 Won!!")
-                break
-            elif(board_position[2]==player2):
-                print("Player 2 Won!!")
-                break
-        if(board_position[0]==board_position[3] and board_position[3]==board_position[6]):
-            flag = 1
-            if(board_position[0]==player1):
-                print("Player 1 Won!!")
-                break
-            elif(board_position[0]==player2):
-                print("Player 2 Won!!")
-                break
-        if(board_position[1]==board_position[4] and board_position[4]==board_position[7]):
-            flag = 1
-            if(board_position[1]==player1):
-                print("Player 1 Won!!")
-                break
-            elif(board_position[1]==player2):
-                print("Player 2 Won!!")
-                break
-        if(board_position[2]==board_position[5] and board_position[5]==board_position[8]):
-            flag = 1
-            if(board_position[2]==player1):
-                print("Player 1 Won!!")
-                break
-            elif(board_position[2]==player2):
-                print("Player 2 Won!!")
-                break
+        if(win_check() == 1):
+            print("Player1 WON!!")
+            exit(0)
         c = c+1
     else:
         break
@@ -147,70 +96,9 @@ while(c<10):
         print('\n'*100)
         display_board()
         player2_move()
-        if(board_position[0]==board_position[1] and board_position[1]==board_position[2]):
-            flag = 1
-            if(board_position[0]==player2):
-                print("Player 2 Won!!")
-                break
-            elif(board_position[0]==player1):
-                print("Player 1 Won!!")
-                break
-        if(board_position[3]==board_position[4] and board_position[4]==board_position[5]):
-            flag = 1
-            if(board_position[3]==player2):
-                print("Player 2 Won!!")
-                break
-            elif(board_position[3]==player1):
-                print("Player 1 Won!!")
-                break
-        if(board_position[6]==board_position[7] and board_position[7]==board_position[8]):
-            flag = 1
-            if(board_position[6]==player2):
-                print("Player 2 Won!!")
-                break
-            elif(board_position[6]==player1):
-                print("Player 1 Won!!")
-                break
-        if(board_position[0]==board_position[4] and board_position[4]==board_position[8]):
-            flag = 1
-            if(board_position[0]==player2):
-                print("Player 2 Won!!")
-                break
-            elif(board_position[0]==player1):
-                print("Player 1 Won!!")
-                break
-        if(board_position[2]==board_position[4] and board_position[4]==board_position[6]):
-            flag = 1
-            if(board_position[2]==player2):
-                print("Player 2 Won!!")
-                break
-            elif(board_position[2]==player1):
-                print("Player 1 Won!!")
-                break
-        if(board_position[0]==board_position[3] and board_position[3]==board_position[6]):
-            flag = 1
-            if(board_position[0]==player2):
-                print("Player 2 Won!!")
-                break
-            elif(board_position[0]==player1):
-                print("Player 1 Won!!")
-                break
-        if(board_position[1]==board_position[4] and board_position[4]==board_position[7]):
-            flag = 1
-            if(board_position[1]==player2):
-                print("Player 2 Won!!")
-                break
-            elif(board_position[1]==player1):
-                print("Player 1 Won!!")
-                break
-        if(board_position[2]==board_position[5] and board_position[5]==board_position[8]):
-            flag = 1
-            if(board_position[2]==player2):
-                print("Player 2 Won!!")
-                break
-            elif(board_position[2]==player1):
-                print("Player 1 Won!!")
-                break
+        if(win_check() == 1):
+            print("Player2 WON!!")
+            exit(0)
         c += 1
     else:
         break
